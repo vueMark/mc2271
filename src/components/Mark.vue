86b86ee8eaea5cf43c13ebcf2bffe8218b4b8014
mark.costales <mcostales@t2s-solutions.com>
2021-01-21 23:24:32 -0500

    finally got it to work

--- src/components/Mark.vue
+++ src/components/Mark.vue
@@ -1,16 +1,73 @@
 <template>
 <div>
-  <h1>{{product}}</h1>
-  <vue-simple-spinner message="Loading..."></vue-simple-spinner>
+  <div class="nav-bar"></div>
+  <div id="app">
+    <div class="product">
+      <div class="product-image">
+        <img :src="image">
+      </div>
+
+      <div class="product-info">
+        <h1>{{product}}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <ul>
+          <li v-for="(detail,counter) in details" :key="counter">{{ detail }}</li>
+        </ul>
+
+        <div v-for="variant in variants"
+            :key="variant.variantId"
+            class="color-box"
+            :style="{backgroundColor: variant.variantColor}"
+            @mouseover="updateProduct(variant.variantImage)">
+
+        </div>
+        <button @click="addtoCart">Add to Cart</button>
+
+        <div class="cart">
+          <p>Cart({{cart}})</p>
+        </div>
+      </div>
+
+      </div>
+
+  </div>
 </div>
 </template>
 
 <script>
+import green from '../assets/vmSocks-green-onWhite.jpg'
+import blue from '../assets/vmSocks-blue-onWhite.jpg'
 export default {
-  data: () => {
+  data() {
   return {
-    product:"Kettlebells",
-    description:"Cannon ball with a handle"
+    product:"Socks",
+    description:"prevents foot from smelling",
+    image: green,
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: green
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: blue
+      }
+    ],
+    cart:0
+  }
+},
+methods:{
+  addtoCart(){
+    this.cart += 1
+  },
+  updateProduct(variantImage){
+    this.image = variantImage
   }
 }
 }
@@ -18,4 +75,101 @@ export default {
 
 <style>
 
+body {
+  font-family: tahoma;
+  color:#282828;
+  margin: 0px;
+}
+
+.nav-bar {
+  background: linear-gradient(-90deg, #84CF6A, #16C0B0);
+  height: 60px;
+  margin-bottom: 15px;
+}
+
+.product {
+  display: flex;
+  flex-flow: wrap;
+  padding: 1rem;
+}
+
+img {
+  border: 1px solid #d8d8d8;
+  width: 70%;
+  margin: 40px;
+  box-shadow: 0px .5px 1px #d8d8d8;
+}
+
+.product-image {
+  width: 80%;
+}
+
+.product-image,
+.product-info {
+  margin-top: 10px;
+  width: 50%;
+}
+
+.color-box {
+  width: 40px;
+  height: 40px;
+  margin-top: 5px;
+}
+
+.cart {
+  margin-right: 25px;
+  float: right;
+  border: 1px solid #d8d8d8;
+  padding: 5px 20px;
+}
+
+button {
+  margin-top: 30px;
+  border: none;
+  background-color: #1E95EA;
+  color: white;
+  height: 40px;
+  width: 100px;
+  font-size: 14px;
+}
+
+.disabledButton {
+  background-color: #d8d8d8;
+}
+
+.review-form {
+  width: 400px;
+  padding: 20px;
+  margin: 40px;
+  border: 1px solid #d8d8d8;
+}
+
+input {
+  width: 100%;
+  height: 25px;
+  margin-bottom: 20px;
+}
+
+textarea {
+  width: 100%;
+  height: 60px;
+}
+
+.tab {
+  margin-left: 20px;
+  cursor: pointer;
+}
+
+.activeTab {
+  color: #16C0B0;
+  text-decoration: underline;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+}
 </style>
