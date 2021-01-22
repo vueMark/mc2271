217bf5d82d0d787c9bd77c5feddff23e58931ab0
mark.costales <mcostales@t2s-solutions.com>
2021-01-21 21:14:23 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mark/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mark from './components/Mark.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Mark
   }
 }
 </script>
---
+++ src/components/Mark.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>{{product}}</h1>
+  <vue-simple-spinner message="Loading..."></vue-simple-spinner>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => {
+  return {
+    product:"Kettlebells",
+    description:"Cannon ball with a handle"
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
